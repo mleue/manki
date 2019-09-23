10df15c3000a65ea7a1d174b095f0f248492a8d0
Michael Leue <domus@posteo.de>
2019-09-23 09:11:43 +0200

    put pygments default css into a data .css file, introduce new cli option to optionally define a custom pygments css file to be used

--- manki/cli.py
+++ manki/cli.py
@@ -4,32 +4,42 @@ import click
 import genanki
 from .io import check_path_exists, check_path_is_dir
 from .note import NotesDirectory, Note
-from .model import DECK
+from .model import DECK, MODEL
+
 # TODO add logging
+dir_path_type = click.Path(exists=True, file_okay=False)
+file_path_type = click.Path(exists=True, file_okay=True)
 
 
 @click.command()
-@click.argument("notes_path")
-@click.option("-o", "--out-path", default=None, type=str)
-@click.option("-m", "--media-path", default=None, type=str)
+@click.argument("notes_path", type=dir_path_type)
+@click.option("-o", "--out-path", default=None, type=dir_path_type)
+@click.option("-m", "--media-path", default=None, type=dir_path_type)
 @click.option("-w", "--tag-whitelist", type=str, multiple=True)
 @click.option("-b", "--title-blacklist", type=str, multiple=True)
 @click.option("-f", "--file-type", type=str, default=(".md",), multiple=True)
+@click.option("-p", "--pygments-css-file", type=file_path_type, default=None)
 def manki_cli(
-    notes_path, out_path, media_path, tag_whitelist, title_blacklist, file_type
+    notes_path,
+    out_path,
+    media_path,
+    tag_whitelist,
+    title_blacklist,
+    file_type,
+    pygments_css_file,
 ):
+    # handle paths
     notes_path = Path(notes_path)
-    out_path = Path(out_path) if out_path is not None else out_path
+    out_path = Path(out_path) if out_path is not None else Path.cwd()
     media_path = Path(media_path) if media_path is not None else media_path
-    try:
-        for p in [notes_path, out_path, media_path]:
-            if p is not None:
-                check_path_exists(p)
-                check_path_is_dir(p)
-    except ValueError as e:
-        click.echo(str(e))
-        return
+    pygments_css_file = (
+        Path(__file__).parent / "pygments_default.css"
+        if pygments_css_file is None
+        else Path(pygments_css_file)
+    )
+    MODEL.css += pygments_css_file.read_text()
 
+    # generate cards and add to deck
     media_file_paths = []
     notes_dir = NotesDirectory(
         notes_path, file_type, tag_whitelist, title_blacklist
@@ -38,7 +48,7 @@ def manki_cli(
         media_file_paths.extend(note.resolve_media_file_paths(media_path))
         DECK.add_note(note.to_genanki_note())
 
+    # create and save a package
     click.echo(media_file_paths)
     pkg = genanki.Package(deck_or_decks=DECK, media_files=media_file_paths)
-    out_path = Path(out_path) if out_path is not None else notes_path
     pkg.write_to_file(out_path / "genanki.apkg")
